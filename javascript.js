964c7d935c4dd1202750c5354ef2f36eecb1527f
Dom <dominicsmith1021@gmail.com>
2018-11-30 10:18:17 -0500

    Organized code a bit

--- javascript.js
+++ javascript.js
@@ -7,59 +7,23 @@ var galleries = {
     "Rutgers Info Widget":["bear.jpg","wolf.jpg","tiger.jpg"]
 };
 
+//Creates shortcut for grabbing elements
+function getElem(id) {
+    return document.getElementById(id);
+};
+
 //The div containing carousel items
 var container = getElem('container');
 
 //Grabbing all children of the container and creating the carousels
-// for (var i = 0; i < container.children.length; i++) {
-//     carousel(container.children[i].children[0],galleries[container.children[i].getAttribute('id')]);
-// };
+/* 
+for (var i = 0; i < container.children.length; i++) {
+    carousel(container.children[i].children[0],galleries[container.children[i].getAttribute('id')]);
+};
+*/
 
 var currGal = 0
 var maxGal = Object.keys(galleries).length;
-
-//Creates all elements needed for each gallery
-function createGalleries() {
-    var name = Object.keys(galleries)[currGal]; //Name of gallery
-    var list = galleries[name]; //Grabs the gallery's array
-    var frame = document.createElement('div'); //Creates container for gallery
-    frame.className = "images";
-    frame.style.backgroundImage = "url('assets/images/"+list[0]+"')"; //Preview of next image in gallery
-    var gallery = document.createElement('img')
-    gallery.className = "fading";
-    gallery.style.height = "100%";
-    gallery.style.width = "100%";
-    gallery.style.zIndex = 5;
-    gallery.src = "assets/images/"+list[list.length-1];
-    var label = document.createElement('div'); //Label for name of gallery
-    label.className = "imageLabel";
-    label.innerHTML = name;
-    label.zIndex = 10;
-    frame.appendChild(gallery);
-    frame.appendChild(label);
-    container.appendChild(frame);
-    var op = 0;
-    //Fades gallery into the document after it's creation
-    setTimeout(function() {
-    var apInt = setInterval(appear,30);
-    function appear() {
-        if (op >= 100) {
-            clearInterval(apInt);
-            carousel(gallery,list);
-        }
-        else {
-            op++;
-            frame.style.opacity=op/100;
-        }
-    };
-    }, 250*currGal)
-currGal++;
-}
-
-//Creates gallery for each array in the galleries object
-for (var i = 0; i < maxGal; i++) {
-    createGalleries();
-};
 
 /* 
 gallery - The img that is fading
@@ -105,7 +69,45 @@ function carousel(gallery,images) {
     gallery.addEventListener('oTransitionEnd', function( event ) {moveCarousel();}, false );
 };
 
-//Creates shortcut for grabbing elements
-function getElem(id) {
-    return document.getElementById(id);
+//Creates all elements needed for each gallery
+function createGalleries() {
+    var name = Object.keys(galleries)[currGal]; //Name of gallery
+    var list = galleries[name]; //Grabs the gallery's array
+    var frame = document.createElement('div'); //Creates container for gallery
+    frame.className = "images";
+    frame.style.backgroundImage = "url('assets/images/"+list[0]+"')"; //Preview of next image in gallery
+    var gallery = document.createElement('img')
+    gallery.className = "fading";
+    gallery.style.height = "100%";
+    gallery.style.width = "100%";
+    gallery.style.zIndex = 5;
+    gallery.src = "assets/images/"+list[list.length-1];
+    var label = document.createElement('div'); //Label for name of gallery
+    label.className = "imageLabel";
+    label.innerHTML = name;
+    label.zIndex = 10;
+    frame.appendChild(gallery);
+    frame.appendChild(label);
+    container.appendChild(frame);
+    var op = 0;
+    //Fades gallery into the document after it's creation
+    setTimeout(function() {
+    var apInt = setInterval(appear,30);
+    function appear() {
+        if (op >= 100) {
+            clearInterval(apInt);
+            carousel(gallery,list);
+        }
+        else {
+            op++;
+            frame.style.opacity=op/100;
+        }
+    };
+    }, 250*currGal)
+currGal++;
+}
+
+//Creates gallery for each array in the galleries object
+for (var i = 0; i < maxGal; i++) {
+    createGalleries();
 };
